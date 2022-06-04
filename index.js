4f1397ad3dae0b2d9c4331df293fa9da18c3fe8d
winston kiprop <winstonekip50@gmail.com>
2022-06-04 12:46:53 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(){}
+function subtract(){}
+function multiply(){}
+function divide(){}
+function add(a,b){
+    return (a+b)
+}
+function subtract(a,b){
+    return(a-b)
+}
+function multiply(a,b){
+    return(a*b)
+}
+function divide(a,b){
+    return(a/b)
+}
+function increment(n){
+    return ++n
+}
+let decrement = (n) => n-=1;
+ 
+
+let makeInt = (n) => parseInt(n, 10);
+
+let preserveDecimal = (n) =>parseFloat(n)
